2904eea80eb4c296fc7ada16ff343457fe8c5ed5
Sunil8 <sunilprience123@gmail.com>
2016-09-18 12:00:26 +0545

    Add files via upload

---
+++ SPHERE.C
@@ -0,0 +1,23 @@
+/*
+Student Name:Sunil Giri
+Subject:Programming Fundamentals
+Roll No.:
+Program:Program to Print Area and Volume OF Sphere.
+Lab No.:03
+Date:18 sep,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+void main(){
+	float radius,area,volume;
+	clrscr();
+	printf("Radius of Sphere:\n");
+	scanf("%f",&radius);
+	area=3.14*4*pow(radius,2);
+	printf("Area of Sphere=%f\n",area);
+	volume=3.14*1.33*pow(radius,3);
+	printf("Volume of Sphere=%f",volume);
+	getch();
+}
