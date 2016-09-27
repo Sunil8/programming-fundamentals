aa657dd60d211bc57da2971a6d4709fa2ecdb3e4
Sunil8 <sunilprience123@gmail.com>
2016-09-27 16:59:40 +0545

    Add files via upload

---
+++ roots of quadratic equation.c
@@ -0,0 +1,27 @@
+/*
+Student Name:Sunil Giri
+Subject:Programming fundamentals
+Roll No:
+Program:Program to ask three coefficients of quadratic equations and find roots.
+Lab No:04
+Date:27 sep,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+void main(){
+    float a,b,c,discriminant,r1,r2;
+    printf("Enter the coefficent of x:");
+    scanf("%f",&a);
+    printf("Enter the coefficent of y:");
+    scanf("%f",&b);
+    printf("Enter the coefficent of z:");
+    scanf("%f",&c);
+    discriminant=pow(b,2)-(4*a*c);
+    printf("discriminant=%f\n",discriminant);
+    r1=(-b+sqrt(discriminant))/(2*a);
+    r2=(-b-sqrt(discriminant))/(2*a);
+    printf("root of quadratic equation=%f,%f",r1,r2);
+    getch();
+}
