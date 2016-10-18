a1b24dff410d5574541f0856337cc35a94974642
Sunil8 <sunilprience123@gmail.com>
2016-10-18 18:46:44 +0545

    Add files via upload

---
+++ inc-decoper.c
@@ -0,0 +1,27 @@
+/* 
+Student Name= Sunil Giri 
+Subject=Programming Fundamentals 
+Roll No.=
+Program=incerement and decreament. 
+Lab No.=08
+Date=18th oct,2016
+*/
+#include<stdio.h>
+//#include<conio.h>
+
+int main (){
+	int a;
+	printf("Enter the Value of a:");
+	scanf("%d",&a);
+	printf("Original value of a=%d\n",a);
+	printf("The post-fix increment(a++) is %d\n",a++);
+	printf("The pre-fix increment(++a) is %d\n",++a);
+	
+	printf("Original value of a=%d\n",a);
+	printf("The post-fix decrement(a--) is %d\n",a--);
+	printf("The pre-fix decrement(--a) is %d\n",--a);
+	//getch();
+	return 0;
+}
+	
+	
