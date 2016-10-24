02d41ddf7fb61afee7b55f81d95adf8617d40c4b
Sunil8 <sunilprience123@gmail.com>
2016-10-24 16:35:04 +0545

    Update oddevenusingter.c

--- oddevenusingter.c
+++ oddevenusingter.c
@@ -13,7 +13,7 @@ Date=24 oct,2016
 
 int main(){
 	int a;
-	printf("Enter first number:\n");
+	printf("Enter a number:\n");
 	scanf("%d",&a);
 	(a%2==0)?printf("%d is even number.\n",a):printf("%d is odd number.\n",a);
 	//getch);
