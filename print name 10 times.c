0e02506a02e71f091482bc83835db2822d20a66a
Sunil8 <sunilprience123@gmail.com>
2016-11-15 10:11:13 +0545

    Add files via upload

---
+++ print name 10 times.c
@@ -0,0 +1,22 @@
+/*Student Name=Sunil Giri
+Subject=Programming Fundamentals 
+Roll No.=
+Program=to print your name 10 times.
+Lab No.=14
+BCS Sem.=1st
+Date=15 nov,2016
+*/
+
+#include<stdio.h>
+//#include<conio.h>
+
+int main(){
+	int i;
+	for(i=0;i<10;i++)
+	printf("Sunil Giri.\n");
+	
+	//getch();
+	return 0;
+}
+	
+	
