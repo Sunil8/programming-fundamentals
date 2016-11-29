b203b9bcea97361689a18c1aab3da000fa3921ac
Sunil8 <sunilprience123@gmail.com>
2016-11-29 11:17:25 +0545

    sum of digits in a number

---
+++ sum of digits of number.c
@@ -0,0 +1,29 @@
+/*Student Name=Sunil Giri
+Subject=Programming Fundamentals 
+Roll No.=
+Program=to find sum of digits of number.
+Lab No.=19
+BCS Sem.=1st
+Date=29 nov,2016
+*/
+
+#include<stdio.h>
+//#include<conio.h>
+
+int main(){
+	int n,rem,sum=0;
+	
+	printf("Enter any number:");
+	scanf("%d",&n);
+	
+	while(n>0)
+	{
+		rem=n%10;
+		sum+=rem;
+		n/=10;
+	}
+	printf("Sum of digits=%d\n",sum);
+	
+	//getch();
+	return 0;
+}
