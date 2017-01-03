bb0de3b97e0559c674b04970bc46cfdc68aa55af
Sunil8 <sunilprience123@gmail.com>
2017-01-03 10:32:39 +0545

    Add files via upload

---
+++ fibonacci series using do-while loop.c
@@ -0,0 +1,31 @@
+/*Student Name=Sunil Giri
+Subject=Programming Fundamentals 
+Roll No.=
+Program=program to print fibonacci series upto n terms.
+Lab No.=24
+BCS Sem.=1st
+Date=jan 3,2017
+*/
+
+#include<stdio.h>
+//#include<conio.h>
+
+
+int main()
+{
+	int n,i=1,a=0,b=1,c=1;
+	
+	printf("Enter number to find fibonacci series upto number terms:");
+	scanf("%d",&n);
+	
+	do
+	{
+		printf("%d\t",c);
+		c=a+b;
+		a=b;
+		b=c;
+		i++;
+	}
+	while(i<=n);
+	
+}
